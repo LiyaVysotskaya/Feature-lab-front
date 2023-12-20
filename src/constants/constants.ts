c047ef72b4859489cad275378eafd621e8a0a6bb
Liya Vysotskaya <Imredrat@yandex.ru>
2023-12-20 14:46:49 +0300

    fix: add constants.ts

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-export const emailRegEx = '^\\S+@\\S+\\.\\S+$';
-export const nameRegEx = '^[A-Za-zА-Яа-яЁё \\-]+$';
-=======
 export const EMAIL_REG_EX = '^\\S+@\\S+\\.\\S+$';
 export const NAME_REG_EX = '^[A-Za-zА-Яа-яЁё \\-]+$';
 export const MIN_LENGTH_NAME = 2;
@@ -10,4 +6,3 @@ export const MIN_LENGTH_EMAIL = 6;
 export const MAX_LENGTH_EMAIL = 60;
 export const MIN_LENGTH_PROJECT = 15;
 export const MAX_LENGTH_PROJECT = 500;
->>>>>>> feat/#26338543_create_popup_connections
