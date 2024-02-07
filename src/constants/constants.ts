927af5ba6ffa4859c13816a832f127a59a16467c
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-02-07 17:29:52 +0400

    feat: add constants for auth and profile routes

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -11,6 +11,11 @@ export const ROUTE_HOME = '/';
 export const ROUTE_COMPETENCIES = '/competencies';
 export const ROUTE_ED_TECH = '/ed-tech';
 export const ROUTE_PRODUCTS = '/products';
+export const ROUTE_PROFILE = '/profile';
+export const ROUTE_LOGIN = '/login';
+export const ROUTE_REGISTER = '/register';
+export const ROUTE_RESET_PASSWORD = '/reset-password';
+export const ROUTE_FORGOT_PASSWORD = '/forgot-password';
 
 // COMPETENCIES subroutes
 export const SUBROUTE_GAMEDEV = `gamedev`;
@@ -18,9 +23,20 @@ export const SUBROUTE_GAMEDEV = `gamedev`;
 // PRODUCTS subroutes
 export const SUBROUTE_DOCSHABLON = `dockshablon`;
 
+// PROFILE subroutes
+export const SUBROUTE_DASHBOARD = `dashboard`;
+export const SUBROUTE_SETTINGS = `settings`;
+export const SUBROUTE_DOCS = `docs`;
+
 // full route paths for subroutes
 // _COMPETENCIES:
 export const ROUTE_GAMEDEV = `${ROUTE_COMPETENCIES}/${SUBROUTE_GAMEDEV}`;
 
 // _PRODUCTS:
-export const ROUTE_DOCSHABLON = `${ROUTE_PRODUCTS}/${SUBROUTE_DOCSHABLON}`;
+export const ROUTE_PRODUCTS_DOCSHABLON = `${ROUTE_PRODUCTS}/${SUBROUTE_DOCSHABLON}`;
+
+// _PROFILE:
+export const ROUTE_PROFILE_DASHBOARD = `${ROUTE_PROFILE}/${SUBROUTE_DASHBOARD}`;
+export const ROUTE_PROFILE_DOCSHABLON = `${ROUTE_PROFILE}/${SUBROUTE_DOCSHABLON}`;
+export const ROUTE_PROFILE_DOCS = `${ROUTE_PROFILE}/${SUBROUTE_DOCS}`;
+export const ROUTE_PROFILE_SETTINGS = `${ROUTE_PROFILE}/${SUBROUTE_SETTINGS}`;
