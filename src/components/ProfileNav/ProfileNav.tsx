f591d4804c11c9d1deac809fa394feb0dfe94ad5
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-02-08 20:22:22 +0400

    feat: add conditional routing menu to profile page

--- src/components/ProfileNav/ProfileNav.tsx
+++ src/components/ProfileNav/ProfileNav.tsx
@@ -1,68 +1,65 @@
 import cl from 'classnames';
-import { NavLink } from 'react-router-dom';
-import { CubeIcon, FolderOpenIcon, GearSixIcon, SkipForwardIcon } from '../ui/icons';
+import { useLocation } from 'react-router-dom';
+import { projects } from '../../_mockData/projectsMockData';
 import {
+  ROUTE_PROFILE,
   ROUTE_PROFILE_DASHBOARD,
   ROUTE_PROFILE_DOCS,
   ROUTE_PROFILE_SETTINGS,
 } from '../../constants/constants';
+import { CardholderIcon, FolderOpenIcon, GearSixIcon, SkipForwardIcon } from '../ui/icons';
 import s from './ProfileNav.module.scss';
+import { ProfileNavLink } from './ProfileNavLink/ProfileNavLink';
 import { ProfileProjectsNav } from './ProfileProjectsNav/ProfileProjectsNav';
 
 export const ProfileNav: React.FC = () => {
-  // const [isSubMenuVisible, setSubMenuVisible] = useState(false);
-  // const location = useLocation();
-  // const navigate = useNavigate();
+  const location = useLocation();
+  const productsPages = projects.map((project) => `${ROUTE_PROFILE}/${project.url}`);
+  const isProductsPage = productsPages.includes(location.pathname);
+  const hasMultipleProducts = productsPages.length > 1;
 
   return (
     <nav aria-label="Меню личного кабинета" className={cl(s.nav)}>
       <ul className={cl(s.list)}>
+        {!hasMultipleProducts && (
+          <li className={cl(s.listItem)}>
+            <ProfileNavLink
+              to={productsPages[0]}
+              icon={<CardholderIcon />}
+              isIconFilled={isProductsPage}
+              text="Проект"
+            />
+          </li>
+        )}
+
+        {hasMultipleProducts && (
+          <li className={cl(s.listItem)}>
+            <ProfileNavLink
+              to={ROUTE_PROFILE_DASHBOARD}
+              icon={<CardholderIcon />}
+              isIconFilled={location.pathname === ROUTE_PROFILE_DASHBOARD || isProductsPage}
+              text="Проекты"
+            />
+          </li>
+        )}
+
+        {hasMultipleProducts && (
+          <li className={cl(s.submenu)}>
+            <ProfileProjectsNav />
+          </li>
+        )}
+
         <li className={cl(s.listItem)}>
-          <NavLink
-            to={ROUTE_PROFILE_DASHBOARD}
-            className={({ isActive }) => cl(s.link, { [s.linkActive]: isActive })}>
-            {({ isActive }) => (
-              <>
-                <CubeIcon filled={isActive} />
-                Главная
-              </>
-            )}
-          </NavLink>
+          <ProfileNavLink to={ROUTE_PROFILE_DOCS} icon={<FolderOpenIcon />} text="Документы" />
         </li>
 
         <li className={cl(s.listItem)}>
-          <ProfileProjectsNav />
+          <ProfileNavLink to={ROUTE_PROFILE_SETTINGS} icon={<GearSixIcon />} text="Профиль" />
         </li>
 
         <li className={cl(s.listItem)}>
-          <NavLink
-            to={ROUTE_PROFILE_DOCS}
-            className={({ isActive }) => cl(s.link, { [s.linkActive]: isActive })}>
-            {({ isActive }) => (
-              <>
-                <FolderOpenIcon filled={isActive} />
-                Документы
-              </>
-            )}
-          </NavLink>
-        </li>
-
-        <li className={cl(s.listItem)}>
-          <NavLink
-            to={ROUTE_PROFILE_SETTINGS}
-            className={({ isActive }) => cl(s.link, { [s.linkActive]: isActive })}>
-            {({ isActive }) => (
-              <>
-                <GearSixIcon filled={isActive} />
-                Профиль
-              </>
-            )}
-          </NavLink>
-        </li>
-
-        <li className={cl(s.listItem)}>
-          <button type="button" className={cl(s.link)}>
-            <SkipForwardIcon /> Выход
+          <button type="button" className={cl(s.button)}>
+            <SkipForwardIcon /> <span className={cl(s.buttonText)}>Выход</span>
           </button>
         </li>
       </ul>
--- src/components/ProfileNav/ProfileProjectsNav/ProfileProjectsNav.tsx
+++ src/components/ProfileNav/ProfileProjectsNav/ProfileProjectsNav.tsx
@@ -1,10 +1,10 @@
 import cl from 'classnames';
 import { FC } from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import { projects } from '../../../_mockData/projectsMockData';
-import { ArrowFatRightIcon, CardholderIcon } from '../../ui/icons';
 import { ROUTE_PROFILE } from '../../../constants/constants';
+import { ArrowFatRightIcon } from '../../ui/icons';
+import { ProfileNavLink } from '../ProfileNavLink/ProfileNavLink';
 import s from './ProfileProjectsNav.module.scss';
 
 interface IProfileProjectsNavProps {
@@ -12,37 +12,17 @@ interface IProfileProjectsNavProps {
 }
 
 export const ProfileProjectsNav: FC<IProfileProjectsNavProps> = ({ className = '' }) => {
-  const location = useLocation();
-
-  const productsPages = projects.map((item) => `${ROUTE_PROFILE}/${item.url}`);
-
-  const isProductsPage = productsPages.includes(location.pathname);
-
   return (
-    <nav aria-label="Проекты" className={cl(s.submenu, className)}>
-      <div
-        className={cl(s.submenuTitle, {
-          [s.submenuLinkActive]: isProductsPage,
-        })}>
-        <CardholderIcon filled={isProductsPage} />
-        Проекты
-      </div>
-      <ul className={cl(s.submenuList)}>
-        {projects.map((item) => (
-          <li className={s.submenuItem} key={uuidv4()}>
-            <NavLink
-              to={`${ROUTE_PROFILE}/${item.url}`}
-              className={({ isActive }) => cl(s.submenuLink, { [s.submenuLinkActive]: isActive })}>
-              {({ isActive }) => (
-                <>
-                  <ArrowFatRightIcon className={s.submenuIcon} filled={isActive} />
-                  {item.projectName}
-                </>
-              )}
-            </NavLink>
-          </li>
-        ))}
-      </ul>
-    </nav>
+    <ul className={cl(s.projects, className)}>
+      {projects.map((item) => (
+        <li className={s.project} key={uuidv4()}>
+          <ProfileNavLink
+            to={`${ROUTE_PROFILE}/${item.url}`}
+            icon={<ArrowFatRightIcon />}
+            text={item.projectName}
+          />
+        </li>
+      ))}
+    </ul>
   );
 };
