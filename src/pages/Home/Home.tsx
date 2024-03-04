03e002c0b1447fd74a79c269dfe66cf98e1ec0a5
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-03-04 21:52:20 +0400

    refactor: update test Btn for Auth in Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,56 +1,23 @@
 import { FC } from 'react';
-import { toast } from 'react-toastify';
-import { getAuth, getUserProfileData } from '../../api/api';
 import { CompetenciesSection } from '../../components/CompetenciesSection/CompetenciesSection';
 import { LabSection } from '../../components/LabSection/LabSection';
 import { Main } from '../../components/Main/Main';
 import { ProductsSection } from '../../components/ProductsSection/ProductsSection';
 import { PromoSection } from '../../components/PromoSection/PromoSection';
 import { Button } from '../../components/ui/Button/Button';
-import { LoginFormData } from '../../types/data';
-import { setStoredAccessToken, setStoredRefreshToken } from '../../utils/localStorageHelpers';
 import s from './Home.module.scss';
+import useAuth from '../../hooks/useAuth';
 
 export const Home: FC = () => {
+  const { signIn } = useAuth();
   const testLoginData = {
     email: 'cherdantsev.p@gmail.com',
     password: 'DU#6ZEB&dXrJ%t',
   };
 
-  const handleLogin = async (loginData: LoginFormData) => {
-    try {
-      const authResponse = await getAuth(loginData);
-      if (authResponse.access && authResponse.refresh) {
-        setStoredAccessToken(authResponse.access);
-        setStoredRefreshToken(authResponse.refresh);
-      } else {
-        // Handle error or clearStore
-      }
-    } catch (error) {
-      console.error('Error during login:', error);
-      // Handle error
-    }
-  };
-
-  const handleGetUser = async () => {
-    const user = await getUserProfileData();
-
-    console.log('user : ', user);
-
-    const toastOption = {
-      position: toast.POSITION.BOTTOM_CENTER,
-      autoClose: 3000,
-    };
-
-    const notifyAuthError = () => toast('ура дали юзера', toastOption);
-
-    notifyAuthError();
-  };
-
   return (
     <Main>
-      <Button text="дай юзера" theme="promo" onClick={() => handleGetUser()} />
-      <Button text="AUTH" theme="promo" onClick={() => handleLogin(testLoginData)} />
+      <Button text="AUTH" theme="promo" onClick={() => signIn(testLoginData)} />
       <PromoSection className={s.promoSection} />
       <CompetenciesSection className={s.pageSection} />
       <LabSection className={s.pageSection} />
