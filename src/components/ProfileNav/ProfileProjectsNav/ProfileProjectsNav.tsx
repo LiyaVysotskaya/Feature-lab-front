84e3800f0e65b3c9571467fdd15e468fb118a300
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-03-06 20:06:35 +0400

    feat: use react queries everywhere and update routing in App

--- src/components/ProfileNav/ProfileProjectsNav/ProfileProjectsNav.tsx
+++ src/components/ProfileNav/ProfileProjectsNav/ProfileProjectsNav.tsx
@@ -1,25 +1,26 @@
 import cl from 'classnames';
 import { FC } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { projects } from '../../../_mockData/projectsMockData';
-import { ROUTE_PROFILE } from '../../../constants/constants';
+import { ROUTE_PROFILE_PROJECTS } from '../../../constants/constants';
+import { TProjectShortInfo } from '../../../types/data';
 import { ArrowFatRightIcon } from '../../ui/icons';
 import { ProfileNavLink } from '../ProfileNavLink/ProfileNavLink';
 import s from './ProfileProjectsNav.module.scss';
 
 interface IProfileProjectsNavProps {
   className?: string;
+  projects: TProjectShortInfo[];
 }
 
-export const ProfileProjectsNav: FC<IProfileProjectsNavProps> = ({ className = '' }) => {
+export const ProfileProjectsNav: FC<IProfileProjectsNavProps> = ({ className = '', projects }) => {
   return (
     <ul className={cl(s.projects, className)}>
       {projects.map((item) => (
         <li className={s.project} key={uuidv4()}>
           <ProfileNavLink
-            to={`${ROUTE_PROFILE}/${item.url}`}
+            to={`${ROUTE_PROFILE_PROJECTS}/${item.id}`}
             icon={<ArrowFatRightIcon />}
-            text={item.projectName}
+            text={item.name}
           />
         </li>
       ))}
--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -1,10 +1,39 @@
-import { FC } from 'react';
-import { Outlet } from 'react-router-dom';
+import { FC, useEffect } from 'react';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { useProjectsQuery } from '../../api/queries';
 import { Main } from '../../components/Main/Main';
 import { ProfileNav } from '../../components/ProfileNav/ProfileNav';
+import {
+  ROUTE_PROFILE,
+  ROUTE_PROFILE_DASHBOARD,
+  ROUTE_PROFILE_PROJECTS,
+  ROUTE_PROFILE_SETTINGS,
+} from '../../constants/constants';
 import s from './ProfilePage.module.scss';
 
 export const ProfilePage: FC = () => {
+  const navigate = useNavigate();
+  const { pathname } = useLocation();
+
+  const { data: projects } = useProjectsQuery();
+
+  useEffect(() => {
+    const fetchData = () => {
+      if (projects && pathname === ROUTE_PROFILE) {
+        const projectsCount = projects.length;
+        if (projectsCount === 0) {
+          navigate(ROUTE_PROFILE_SETTINGS);
+        } else if (projectsCount === 1) {
+          navigate(`${ROUTE_PROFILE_PROJECTS}/${projects[0].id}`);
+        } else {
+          navigate(ROUTE_PROFILE_DASHBOARD);
+        }
+      }
+    };
+
+    fetchData();
+  });
+
   return (
     <Main className={s.profile}>
       <ProfileNav />
