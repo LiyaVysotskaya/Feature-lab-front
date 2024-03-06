2955a952537753080a129437ee13e0e900f00979
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-03-06 20:02:43 +0400

    refactor: update hook useAuth

--- src/atoms/userProfileAtom.ts
+++
@@ -1,4 +0,0 @@
-import { atom } from 'jotai';
-import { UserProfile } from '../types/data';
-
-export const userProfileAtom = atom<UserProfile | null>(null);
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,27 +1,23 @@
 import { useAtom } from 'jotai';
-import { useEffect } from 'react';
-import { getAuth, getUserProfileData, postRegData } from '../api/api';
+import { getAuth, postRegData } from '../api/api';
 import { isAuthAtom } from '../atoms/isAuthAtom';
-import { userProfileAtom } from '../atoms/userProfileAtom';
+import queryClient from '../query-client';
 import { LoginFormData, RegFormData } from '../types/data';
 import {
   clearAllStoredTokens,
-  getStoredAccessToken,
   setStoredAccessToken,
   setStoredRefreshToken,
 } from '../utils/localStorageHelpers';
 
 // Custom hook for handling authentication
 const useAuth = () => {
-  // State variables for authentication status and user profile
-  const [isAuth, setIsAuth] = useAtom(isAuthAtom);
-  const [, setUserProfile] = useAtom(userProfileAtom);
+  const [, setIsAuth] = useAtom(isAuthAtom);
 
   const signOut = () => {
     // Clear all stored tokens and reset state variables
     clearAllStoredTokens();
     setIsAuth(false);
-    setUserProfile(null);
+    queryClient.removeQueries();
   };
 
   const signIn = async (loginData: LoginFormData) => {
@@ -31,16 +27,12 @@ const useAuth = () => {
         setStoredAccessToken(authResponse.access);
         setStoredRefreshToken(authResponse.refresh);
         setIsAuth(true);
-        // After successful sign-in, fetch and set user profile data
-        const userData = await getUserProfileData();
-        setUserProfile(userData);
       } else {
         signOut();
       }
     } catch (error) {
       console.error('Error during login:', error);
       // Handle error
-      signOut();
     }
   };
 
@@ -52,36 +44,8 @@ const useAuth = () => {
     }
   };
 
-  useEffect(() => {
-    // Function to check authentication status
-    const checkAuth = async () => {
-      const accessToken = getStoredAccessToken();
-
-      if (accessToken) {
-        try {
-          // Fetch user profile data using access token
-          const userData = await getUserProfileData();
-          // Set authentication status to true and store user profile data
-          setIsAuth(true);
-          setUserProfile(userData);
-        } catch (error) {
-          // If error occurs during data fetching, log the error, clear tokens, and reset authentication status
-          console.error('Error fetching user data:', error);
-          signOut();
-        }
-      } else {
-        // If no access token exists, set authentication status to false and clear user profile
-        setIsAuth(false);
-        setUserProfile(null);
-      }
-    };
-
-    // Call the checkAuth function when the component mounts
-    checkAuth();
-  }, []);
-
   // Return authentication status and user profile data
-  return { isAuth, signOut, signIn, signUp };
+  return { signOut, signIn, signUp };
 };
 
 export default useAuth;
