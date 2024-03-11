c7271e80a5731454853cf99cf710bd5adf02f848
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-03-11 15:34:18 +0400

    refactor: move date convert functions to extrnal file as helpers

--- src/components/ProjectStagesSection/CurrentStage/CurrentStage.tsx
+++ src/components/ProjectStagesSection/CurrentStage/CurrentStage.tsx
@@ -1,7 +1,7 @@
 import cl from 'classnames';
-import { format, parseISO } from 'date-fns';
 import { FC } from 'react';
 import { TProjectStage } from '../../../types/data';
+import { convertDateToShortFormat } from '../../../utils/dateConvertHelpers';
 import { Text } from '../../ui/Text/Text';
 import s from './CurrentStage.module.scss';
 
@@ -11,13 +11,6 @@ type CurrentStageProps = {
 };
 
 export const CurrentStage: FC<CurrentStageProps> = ({ className = '', stage }) => {
-  const convertAndFormatDate = (dateString: string) => {
-    // Parse the ISO 8601 formatted date string to a Date object
-    const date = parseISO(dateString);
-    // Format the date using date-fns
-    return format(date, 'dd.MM.yyyy');
-  };
-
   return (
     <div className={cl(s.currentStage, className)}>
       <div className={cl(s.column)}>
@@ -28,7 +21,7 @@ export const CurrentStage: FC<CurrentStageProps> = ({ className = '', stage }) =
           {stage.name}
         </Text>
         <Text view="gost-1" tag="p" className={cl(s.date)}>
-          срок выполнения: {convertAndFormatDate(stage.end_date)}
+          срок выполнения: {convertDateToShortFormat(stage.end_date)}
         </Text>
       </div>
       <Text view="gost-2" tag="p" className={cl(s.info)}>
