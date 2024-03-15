5801acf8e29dfb8c27d32abf3d35aedf78eba9f2
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-03-15 23:14:51 +0400

    feat: update TeamSlider to TeamSliderSection

---
+++ src/components/TeamSliderSection/CarouselBtn/CarouselBtn.tsx
@@ -0,0 +1,45 @@
+import { FC } from 'react';
+import cl from 'classnames';
+import { ArrowInCircleIcon } from '../../ui/icons';
+import s from './CarouselBtn.module.scss';
+
+interface IProps {
+  onClick?: () => void;
+  customId?: string;
+  direction?: 'left' | 'right';
+}
+
+export const CarouselBtn: FC<IProps> = ({
+  onClick = () => {},
+  customId = '',
+  direction = 'right',
+}) => {
+  if (customId) {
+    return <button onClick={onClick} type="button" id={customId} className={cl(s.isHidden)} />;
+  }
+
+  if (!direction) return null;
+
+  const onBtnClick = () => {
+    const element = document.getElementById(
+      direction === 'left' ? 'TeamSliderBtnLeft' : 'TeamSliderBtnRight',
+    );
+    if (element) {
+      element.click();
+      onClick();
+    }
+    onClick();
+  };
+
+  return (
+    <button onClick={onBtnClick} type="button" id={customId}>
+      <ArrowInCircleIcon
+        reverse={direction === 'left'}
+        className={cl(s.btn, {
+          [s.btnLeft]: direction === 'left',
+          [s.btnRight]: direction === 'right',
+        })}
+      />
+    </button>
+  );
+};
--- src/pages/CompetencePage/CompetencePage.tsx
+++ src/pages/CompetencePage/CompetencePage.tsx
@@ -1,19 +1,28 @@
 import { FC } from 'react';
+import { useParams } from 'react-router';
+import { useCompetenceQuery } from '../../api/queries';
 import { LeadSection } from '../../components/LeadSection/LeadSection';
 import { Main } from '../../components/Main/Main';
 import { PageTitle } from '../../components/PageTitle/PageTitle';
 import { ProductsSection } from '../../components/ProductsSection/ProductsSection';
+import { TeamSliderSection } from '../../components/TeamSliderSection/TeamSliderSection';
 import s from './CompetencePage.module.scss';
-import { TeamSlider } from '../../components/TeamSlider/TeamSlider';
 
 export const CompetencePage: FC = () => {
+  const { competenceSlug } = useParams();
+  const { data: competence, isLoading, isRefetching } = useCompetenceQuery(competenceSlug);
+
+  if (isLoading || isRefetching || !competence) {
+    return null;
+  }
+
   return (
     <Main>
       <PageTitle className={s.pageTitle} pageTitle="Gamedev" subTitle="Расшифровка аббревиатуры" />
 
       <LeadSection className={s.pageSection} />
 
-      <TeamSlider className={s.pageSection} />
+      <TeamSliderSection className={s.pageSection} team={competence.employees} />
 
       <ProductsSection className={s.pageSection} title="Портфолио" />
     </Main>
