a90787d08fb9072b7f6bd1def86548053fc6b56c
Liya Vysotskaya <Imredrat@yandex.ru>
2024-03-18 16:21:22 +0300

    feat: popupConfirmLogout for mobile Nav

--- src/components/ProfileNav/ProfileNavMobile/ProfileNavMobile.tsx
+++ src/components/ProfileNav/ProfileNavMobile/ProfileNavMobile.tsx
@@ -1,25 +1,29 @@
 import cl from 'classnames';
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { useState } from 'react';
 import { useProjectsQuery } from '../../../api/queries';
 import {
-  ROUTE_HOME,
   ROUTE_PROFILE,
   ROUTE_PROFILE_DASHBOARD,
   ROUTE_PROFILE_DOCS,
   ROUTE_PROFILE_PROJECTS,
   ROUTE_PROFILE_SETTINGS,
 } from '../../../constants/routesConstants';
-import useAuth from '../../../hooks/useAuth';
 import { CardholderIcon, FolderOpenIcon, GearSixIcon, SkipForwardIcon } from '../../ui/icons';
 import { ProfileNavLink } from '../ProfileNavLink/ProfileNavLink';
 import s from './ProfileNavMobile.module.scss';
+import { PopupConfirmLogout } from '../../PopupConfirmLogout/PopupConfirmLogout';
 
 export const ProfileNavMobile: React.FC = () => {
+  const [isPopupConfirmLogoutOpen, setPopupConfirmLogoutOpen] = useState(false);
+
   const location = useLocation();
-  const navigate = useNavigate();
-  const { signOut } = useAuth();
 
   const { data: projects, isLoading } = useProjectsQuery();
+
+  const closePopupConfirmLogout = () => {
+    setPopupConfirmLogoutOpen(false);
+  };
 
   if (isLoading || !projects) {
     return null;
@@ -35,50 +39,50 @@ export const ProfileNavMobile: React.FC = () => {
   }
 
   return (
-    <nav aria-label="Меню личного кабинета" className={cl(s.nav)}>
-      <ul className={cl(s.list)}>
-        {PojectsCount === 1 && (
+    <>
+      <nav aria-label="Меню личного кабинета" className={cl(s.nav)}>
+        <ul className={cl(s.list)}>
+          {PojectsCount === 1 && (
+            <li className={cl(s.listItem)}>
+              <ProfileNavLink
+                to={projectsPages[0]}
+                icon={<CardholderIcon />}
+                isIconFilled={isProjectPage}
+                text="Проект"
+              />
+            </li>
+          )}
+
+          {PojectsCount > 1 && (
+            <li className={cl(s.listItem)}>
+              <ProfileNavLink
+                to={ROUTE_PROFILE_DASHBOARD}
+                icon={<CardholderIcon />}
+                isIconFilled={location.pathname === ROUTE_PROFILE_DASHBOARD || isProjectPage}
+                text="Проекты"
+              />
+            </li>
+          )}
+
           <li className={cl(s.listItem)}>
-            <ProfileNavLink
-              to={projectsPages[0]}
-              icon={<CardholderIcon />}
-              isIconFilled={isProjectPage}
-              text="Проект"
-            />
+            <ProfileNavLink to={ROUTE_PROFILE_DOCS} icon={<FolderOpenIcon />} text="Документы" />
           </li>
-        )}
 
-        {PojectsCount > 1 && (
           <li className={cl(s.listItem)}>
-            <ProfileNavLink
-              to={ROUTE_PROFILE_DASHBOARD}
-              icon={<CardholderIcon />}
-              isIconFilled={location.pathname === ROUTE_PROFILE_DASHBOARD || isProjectPage}
-              text="Проекты"
-            />
+            <ProfileNavLink to={ROUTE_PROFILE_SETTINGS} icon={<GearSixIcon />} text="Профиль" />
           </li>
-        )}
 
-        <li className={cl(s.listItem)}>
-          <ProfileNavLink to={ROUTE_PROFILE_DOCS} icon={<FolderOpenIcon />} text="Документы" />
-        </li>
-
-        <li className={cl(s.listItem)}>
-          <ProfileNavLink to={ROUTE_PROFILE_SETTINGS} icon={<GearSixIcon />} text="Профиль" />
-        </li>
-
-        <li className={cl(s.listItem)}>
-          <button
-            type="button"
-            className={cl(s.button)}
-            onClick={() => {
-              navigate(ROUTE_HOME);
-              signOut();
-            }}>
-            <SkipForwardIcon /> <span className={cl(s.buttonText)}>Выход</span>
-          </button>
-        </li>
-      </ul>
-    </nav>
+          <li className={cl(s.listItem)}>
+            <button
+              type="button"
+              className={cl(s.button)}
+              onClick={() => setPopupConfirmLogoutOpen(true)}>
+              <SkipForwardIcon /> <span className={cl(s.buttonText)}>Выход</span>
+            </button>
+          </li>
+        </ul>
+      </nav>
+      <PopupConfirmLogout onClose={closePopupConfirmLogout} isOpen={isPopupConfirmLogoutOpen} />
+    </>
   );
 };
