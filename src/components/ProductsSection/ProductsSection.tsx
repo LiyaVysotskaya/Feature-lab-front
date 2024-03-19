4f411435c80f26e45ea342b351c63d99405581c7
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-03-19 23:53:22 +0400

    feat: ProductsSection works with real data

--- src/components/ProductsSection/ProductsSection.tsx
+++ src/components/ProductsSection/ProductsSection.tsx
@@ -1,32 +1,56 @@
 import cl from 'classnames';
 import { FC } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { products } from '../../_mockData/productsMockData';
 import { SectionTitle } from '../SectionTitle/SectionTitle';
 import s from './ProductsSection.module.scss';
+import { useProductsQuery } from '../../api/queries';
 
 export interface IProductsSectionProps {
   className?: string;
   title: string;
+  competenceId?: string;
 }
 
-export const ProductsSection: FC<IProductsSectionProps> = ({ className = '', title }) => {
+export const ProductsSection: FC<IProductsSectionProps> = ({
+  className = '',
+  title,
+  competenceId = '',
+}) => {
+  const { data: products, isLoading } = useProductsQuery();
+
+  if (isLoading || !products) {
+    return null;
+  }
+
+  // filter products by competenceId
+  const filteredProducts = products.filter((product) => {
+    if (competenceId === '') {
+      return product;
+    }
+    return product.competence === competenceId ? product : null;
+  });
+
+  if (filteredProducts.length === 0) {
+    return null;
+  }
+
   return (
     <section className={cl(s.productsSection, className)}>
       <SectionTitle text={title} />
 
       <ul className={s.list}>
-        {products.map((product) => (
+        {filteredProducts.map((product) => (
           <li className={cl(s.product)} key={uuidv4()}>
             <div className={s.productHeader}>
-              <p className={s.productTitle}>{product.title}</p>
-              <p className={s.productInfo}>{product.info}</p>
+              <p className={s.productTitle}>{product.name}</p>
+              <p className={s.productInfo}>{product.description}</p>
             </div>
             <div className={s.productFooter}>
-              <p>{product.labels}</p>
-              <p className={s.divider}>/</p>
-              <a href="/" className={s.productLink}>
-                ссылка на сайт
+              {product.tags.map((tag) => (
+                <p className={s.productTag} key={uuidv4()}>{`${tag} /`}</p>
+              ))}
+              <a href={product.url} className={s.productLink}>
+                ссылка
               </a>
             </div>
           </li>
