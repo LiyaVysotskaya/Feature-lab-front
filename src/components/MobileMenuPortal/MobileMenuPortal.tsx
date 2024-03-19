9f0ec956c9834b0ead96643f708686c708ba7b4d
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-03-19 14:33:13 +0400

    feat: mobile menu is closing when location changes

--- src/components/MobileMenuPortal/MobileMenuPortal.tsx
+++ src/components/MobileMenuPortal/MobileMenuPortal.tsx
@@ -1,6 +1,7 @@
 import cl from 'classnames';
 import { FC, useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
+import { useLocation } from 'react-router';
 import { MobileMenu } from '../MobileMenu/MobileMenu';
 import s from './MobileMenuPortal.module.scss';
 
@@ -8,8 +9,13 @@ type Props = {};
 
 const MobileMenuPortal: FC<Props> = () => {
   const [isNavMobileOpen, setIsNavMobileOpen] = useState(true);
-
+  const location = useLocation();
   const popupRoot = document.getElementById('root');
+
+  useEffect(() => {
+    // Close the menu when the location changes
+    setIsNavMobileOpen(false);
+  }, [location]);
 
   // Add class to body to disable scrollbar when the popup is open
   useEffect(() => {
