668434cb28a3df6d6b040ca728cf08dd5716e627
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-05-03 18:48:36 +0400

    fix: sort project stages before layout

--- src/pages/ProfilePage/ProfileProject/ProfileProject.tsx
+++ src/pages/ProfilePage/ProfileProject/ProfileProject.tsx
@@ -19,11 +19,13 @@ export const ProfileProject: FC<IProps> = ({ className = '' }) => {
     return null;
   }
 
+  const sortedStages = project?.stages.sort((a, b) => a.stage_num - b.stage_num);
+
   return (
     <div className={cl(s.project, className)}>
       <ProjectInfoSection project={project} className={s.projectSection} />
 
-      <ProjectStagesSection projectStages={project.stages} className={s.projectSection} />
+      <ProjectStagesSection projectStages={sortedStages} className={s.projectSection} />
 
       <DocumentsSection
         title={`Документы по ${project.name}`}
