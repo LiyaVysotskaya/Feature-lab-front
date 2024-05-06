f688c93d4012e89a59f24caf63da62953402d657
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-05-06 14:16:45 +0400

    fix: fetch correctly project logo link

--- src/components/ProjectInfoSection/ProjectInfoSection.tsx
+++ src/components/ProjectInfoSection/ProjectInfoSection.tsx
@@ -1,5 +1,6 @@
 import cl from 'classnames';
 import { FC } from 'react';
+import { API_BASE_URL } from '../../constants/externalLinks';
 import { TProjectFullInfo } from '../../types/profileData';
 import s from './ProjectInfoSection.module.scss';
 
@@ -11,6 +12,7 @@ type IProps = {
 export const ProjectInfoSection: FC<IProps> = ({ className = '', project }) => {
   const manager = project.managers[0];
   const managerName = `${manager.first_name} ${manager.last_name}`;
+  const logoLink = `${API_BASE_URL.slice(0, -1)}${project.logo}`;
 
   return (
     <section className={cl(s.section, className)}>
@@ -26,7 +28,7 @@ export const ProjectInfoSection: FC<IProps> = ({ className = '', project }) => {
       <p className={s.description}>{project.description}</p>
 
       <div className={s.logoContainer}>
-        {project.logo && <img className={s.logo} src={project.logo} alt="Лого" />}
+        {project.logo && <img className={s.logo} src={logoLink} alt="Лого" />}
       </div>
     </section>
   );
