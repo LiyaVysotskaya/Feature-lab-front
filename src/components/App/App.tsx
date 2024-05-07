12f8c02821c9d5d21d410db75db61050c2ee638e
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-05-07 21:45:59 +0400

    refactor: footer visability logic in app.ts

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,15 @@
+import { useIsFetching } from '@tanstack/react-query';
 import { useAtom } from 'jotai';
 import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { useIsFetching } from '@tanstack/react-query';
 import { useUserProfileQuery } from '../../api/queries';
 import { isAuthAtom } from '../../atoms/isAuthAtom';
+import {
+  ROUTE_CHANGE_PASSWORD,
+  ROUTE_ERROR_404,
+  ROUTE_ERROR_500,
+  ROUTE_PROFILE,
+} from '../../constants/routesConstants';
 import { AppRouter } from '../../router';
 import Footer from '../Footer/Footer';
 import { Header } from '../Header/Header';
@@ -16,6 +22,13 @@ const App: React.FC = () => {
   const [, setIsAuth] = useAtom(isAuthAtom);
   const { pathname } = useLocation();
   const isFetching = useIsFetching();
+
+  const isFooterVisible = !(
+    pathname === ROUTE_CHANGE_PASSWORD ||
+    pathname.startsWith(ROUTE_PROFILE) ||
+    pathname === ROUTE_ERROR_500 ||
+    pathname === ROUTE_ERROR_404
+  );
 
   const { data: userData } = useUserProfileQuery();
 
@@ -44,7 +57,7 @@ const App: React.FC = () => {
 
         <CookiesToastContainer />
       </div>
-      {!isFetching && <Footer />}
+      {!isFetching && isFooterVisible && <Footer />}
     </div>
   );
 };
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,5 @@
 import cn from 'classnames';
-import { FC, useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { FC } from 'react';
 import {
   COMPANY_ADDRESS_FIRST_RAW,
   COMPANY_ADDRESS_SECOND_RAW,
@@ -11,36 +10,10 @@ import {
   COMPANY_NAME,
   COMPANY_PHONE,
 } from '../../constants/companyDetails';
-import {
-  ROUTE_CHANGE_PASSWORD,
-  ROUTE_ERROR_404,
-  ROUTE_ERROR_500,
-  ROUTE_PROFILE,
-} from '../../constants/routesConstants';
 import { FormFooter } from './FormFooter/FormFooter';
 import s from './Footer.module.scss';
 
 const Footer: FC = () => {
-  const [showFooter, setShowFooter] = useState(true);
-  const { pathname } = useLocation();
-
-  const shouldShowFooter = () => {
-    return !(
-      pathname === ROUTE_CHANGE_PASSWORD ||
-      pathname.startsWith(ROUTE_PROFILE) ||
-      pathname === ROUTE_ERROR_500 ||
-      pathname === ROUTE_ERROR_404
-    );
-  };
-
-  useEffect(() => {
-    setShowFooter(shouldShowFooter());
-  }, [pathname]);
-
-  if (!showFooter) {
-    return null;
-  }
-
   return (
     <footer className={s.footer}>
       <div className={s.footerContainer}>
