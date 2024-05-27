6802842f13734d0576fc5a1631dacf501c8e4b88
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-05-27 13:59:47 +0400

    refactor: use helper func resizeInputFont in FormContact

--- src/components/PopupContact/FormContact/FormContact.tsx
+++ src/components/PopupContact/FormContact/FormContact.tsx
@@ -11,6 +11,7 @@ import {
   MIN_LENGTH_PROJECT,
   NAME_REG_EX,
 } from '../../../constants/formConstants';
+import { resizeInputFont } from '../../../utils/formHelpers';
 import { useFormAndValidation } from '../../../utils/hooks/useFormAndValidation';
 import { PopupPrivacyPolicy } from '../../PopupPrivacyPolicy/PopupPrivacyPolicy';
 import { RoundButton } from '../../ui/RoundButton/RoundButton';
@@ -30,33 +31,7 @@ export const FormContact: FC = () => {
   });
 
   const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const input = e.target;
-    const inputStyles = getComputedStyle(input);
-
-    // Access the value of the --input-font-size CSS variable
-    const originFontSize = parseInt(inputStyles.getPropertyValue('--input-font-size'), 10);
-
-    // Access the value of the minimum font size CSS variable
-    const minFontSize = parseInt(inputStyles.getPropertyValue('--input-font-size-min'), 10);
-
-    // Retrieve the current font size
-    const currentFontSize = parseInt(inputStyles.fontSize, 10);
-
-    const textLength = input.value.length;
-
-    // Calculate the new font size based on text length
-    let newFontSize = currentFontSize;
-    if (textLength > 20) {
-      // Decrease font size smoothly, but ensure it doesn't go below the minimum
-      const fontSizeDifference = Math.max(textLength - 20, 0);
-      newFontSize = Math.max(originFontSize - fontSizeDifference, minFontSize);
-    } else {
-      newFontSize = originFontSize;
-    }
-
-    // Apply the new font size to the input
-    input.style.fontSize = `${newFontSize}px`;
-
+    resizeInputFont(e);
     handleChange(e);
   };
 
