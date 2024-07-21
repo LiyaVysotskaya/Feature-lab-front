5fe38eec3d2a529f64e4d7f7904cf3c1f1c72035
Pavel Cherdantsev <cherdantsev.p@gmail.com>
2024-07-22 00:27:38 +0400

    add offline caching in local storage for queries

--- src/query-client.tsx
+++ src/query-client.tsx
@@ -1,4 +1,6 @@
 import { QueryClient } from '@tanstack/react-query';
+// import { persistQueryClient } from '@tanstack/react-query-persist-client';
+// import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -9,5 +11,16 @@ const queryClient = new QueryClient({
     },
   },
 });
+
+// localstorage persisting disabled for easy testing
+
+// const localStoragePersister = createSyncStoragePersister({
+//   storage: window.localStorage,
+// });
+
+// persistQueryClient({
+//   queryClient,
+//   persister: localStoragePersister,
+// });
 
 export default queryClient;
